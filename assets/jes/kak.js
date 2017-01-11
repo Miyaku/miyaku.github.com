f3b0d945a810229b26699a4dd24818c7a3a7f99f
M <miya@teknik.io>
2017-01-11 11:25:22 +0700

    Create kak.js

---
+++ assets/jes/kak.js
@@ -0,0 +1,14 @@
+<!--
+date = new Date();
+if ((date.getUTCHours() - date.getHours()) != 0) (date = date.getHours())
+else (date = (8 + date.getHours()));
+if (date < 4) {alert("Begadang itu tidak baik kalau tidak ada gunanya")}
+else {if (date < 11) {alert("Sudah sholat subuh?")}
+else {if (date < 16) {alert("Sudah sholat dzuhur?")}
+else {if (date < 20) {alert("Sudah solat Asyar?")}
+else {alert("Sudah sholat isya belum?")}
+}
+}
+}
+//-->
+
