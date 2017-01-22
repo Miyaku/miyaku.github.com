94933cf4d148914dd8ee7229cdd5fac4dd767826
Miya <ciutirc@gmail.com>
2017-01-22 16:33:02 +0700

    Update time.js

--- assets/jes/time.js
+++ assets/jes/time.js
@@ -1,21 +1,17 @@
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
+(function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
     }
-    return i;
-}
 
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-    t = setTimeout(function () {
-        startTime()
-    }, 500);
-}
-startTime();
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
